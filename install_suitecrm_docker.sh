d78e1936d262c96bdbf0c4576bbd51e3aeb2cfcb
Dimitro Pukalov <dimitro.pukalov@compacer.com>
2025-03-06 16:31:04 +0100

    fixed typo

--- install_suitecrm_docker.sh
+++ install_suitecrm_docker.sh
@@ -48,6 +48,7 @@ install_dependencies() {
         ubuntu|debian)
             if [ ! -f /etc/apt/keyrings/charm.gpg ] || [ ! -f /usr/bin/gum ]; then
                 sudo mkdir -p /etc/apt/keyrings
+                apt install curl
                 curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
                 echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
                 sudo apt update && sudo apt install -y gum
